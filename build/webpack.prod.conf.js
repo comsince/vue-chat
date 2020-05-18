9ed0e379b4ee3c742e1bd5ae81e76bc477d30fce
comsince <liaojinlong@meizu.com>
2020-05-18 18:18:49 +0800

    增加webpack prod icon 配置

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -53,6 +53,7 @@ var webpackConfig = merge(baseWebpackConfig, {
       filename: config.build.index,
       template: 'index.html',
       inject: true,
+      favicon: path.resolve('favicon.ico'),
       minify: {
         removeComments: true,
         collapseWhitespace: true,
