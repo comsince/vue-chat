4687a08b4db81516e4b6750343fa00615da76e22
Renfei <ljlong_2008@126.com>
2021-01-04 21:53:40 +0800

    Update index.js
    修改websocket地址

--- src/websocket/index.js
+++ src/websocket/index.js
@@ -63,7 +63,7 @@ export default class VueWebSocket {
         this.heartbeatTimeout = HEART_BEAT_INTERVAL;
         this.reconnectInterval = RECONNECT_INTERVAL;
         this.binaryType = BINTRAY_TYPE;
-        this.url = WS_PROTOCOL + '://' + WS_IP + ':'+ WS_PORT;
+        this.url = WS_PROTOCOL + '://' + WS_IP ;
         this.initHandlerList();
         this.connect(true);
     }
